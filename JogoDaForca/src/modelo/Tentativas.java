26f1789d15bc94e014933049f7fb2951c905a5e6
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-05-30 16:15:59 -0300

    Classe Tentativas criada

---
+++ JogoDaForca/src/modelo/Tentativas.java
@@ -0,0 +1,31 @@
+
+package modelo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Tentativas {
+    
+    public static final int maximo = 6;
+    private int atual;
+    
+    public Tentativas() {
+        atual = maximo;
+    }
+    
+    public int atual(){
+        return atual;
+    }
+    
+    public void errou(){
+        if(atual > 0){
+            atual--;       
+        }
+    }
+    
+    public boolean acabou(){
+        return atual == 0;
+    }
+    
+}
