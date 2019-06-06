ea0f865fa76dc05c194664fdbf1432ab73f09de3
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-06-06 16:58:09 -0300

    Interfaces de observador adicionadas

---
+++ JogoDaForca/src/modelo/interfaces/ObservadorLetrasUsadas.java
@@ -0,0 +1,12 @@
+
+package modelo.interfaces;
+
+import modelo.Letra;
+
+/**
+ *
+ * @author João e Marcelo
+ */
+public interface ObservadorLetrasUsadas {
+    public void novaLetra(Letra l);
+}
---
+++ JogoDaForca/src/modelo/interfaces/ObservadorSegredo.java
@@ -0,0 +1,12 @@
+
+package modelo.interfaces;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author João e Marcelo
+ */
+public interface ObservadorSegredo {
+    public void revelar(ArrayList<Integer> indices, char c);
+}
