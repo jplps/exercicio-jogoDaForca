c2d5fcf01e812c022f7e5a12559150da39482c1a
Alexandre de Limas Santana <alexandre.limas.santana@gmail.com>
2019-06-13 16:30:30 -0300

    Criada a interface para nova partida

---
+++ JogoDaForca/src/controle/CriadorPartidas.java
@@ -0,0 +1,14 @@
+package controle;
+
+/**
+ * Interface pública para criar partidas.
+ *
+ * @author João, Carlos e Leandro
+ */
+public interface CriadorPartidas {
+
+    /**
+     * Cria nova partida.
+     */
+    public void novaPartida();
+}
